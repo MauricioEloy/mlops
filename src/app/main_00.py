df30f2ba8d28afa5b3df8d9f517c781ab5ef382d
MauricioEloy <profmauricioeloy@gmail.com>
2021-03-01 07:27:39 -0300

    first commit

---
+++ src/app/main_00.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+from textblob import TextBlob
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv("../../data/processed/casas.csv")
+colunas = ['tamanho','ano','garagem']
+X = df.drop('preco', axis=1)
+y = df['preco']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+modelo = LinearRegression()
+modelo.fit(X_train, y_train)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Minha primeira API."
+
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(to='en')
+    polaridade = tb_en.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
+@app.route('/cotacao/', methods=['POST'])
+#@basic_auth.required
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+    return jsonify(preco=preco[0])
+
+
+app.run(debug=True)
+
+
+
